01a9fdeb3025cc9b6a62f55c02d0ade17e6afe0d
kjg <k_1@kakao.com>
2023-04-22 20:52:25 +0900

    2-1 변수 타입

---
+++ ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,27 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		//변수 선언
+		int age;
+		//변수에 초기값 15를 저장
+		age = 15;
+		System.out.println(age);
+		
+		//변수를 선언하고 초기값을 저장
+		int score =90;
+		
+		int value = 5;
+		int result = value + 10;
+		//예제
+		//변수 salaly 선언
+		int salaly = 600;
+		
+		//연산 결과를 변수 numver의 초기값으로 대입
+		int salalyman = salaly +10;
+		
+		//변수 slalalyman 값을 읽고 콘솔에 출력
+		System.out.println(salalyman);
+	}
+}
