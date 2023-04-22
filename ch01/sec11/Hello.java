f7bcc5113c8a9ba80ece3f9f893307506a265088
kjg <k_1@kakao.com>
2023-04-22 19:54:11 +0900

    자바시작하기

---
+++ ch01/sec11/Hello.java
@@ -0,0 +1,22 @@
+package ch01.sec11;
+
+public class Hello {
+	/**
+	 * @author blueskii
+	 * @param args
+	 */
+	
+	/*
+	 장제목 :1장 자바 시작하기
+	 작성일 :2022 .04. 22
+	 */
+	
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java2");
+		System.out.println("Hello, Java2");
+	
+	}
+
+}
